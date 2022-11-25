6a7de7e3b4fd5e6a03d46aff7ca73db7aee0ec92
ZBreno <brenosoares9533@gmail.com>
2022-11-24 21:58:04 -0300

    adicionando carousel nas paginas de estudante

--- script/index.js
+++ script/index.js
@@ -6,3 +6,12 @@ new Glider(document.querySelector(".glider"),{
         next: ".glider-next",
     }
 })
+
+new Glider(document.querySelector(".glider1"),{
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: {
+        prev: ".prev-favorite",
+        next: ".next-favorite",
+    }
+})
