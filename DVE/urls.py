c618f9e712dcd9059cf735f7b0c2328fac7d9727
Jose Neto <ze.neto429@gmail.com>
2022-12-11 15:06:06 -0300

    aplicando Django

--- DVE/urls.py
+++ DVE/urls.py
@@ -14,13 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("authentication.urls")),
-    path('', include("carousel.urls")),
-    path('', include("forms.urls")),
-    path('', include("ladingPage.urls")),
-    path('', include("profiles.urls")),
 ]
